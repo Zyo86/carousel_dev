95e4eb51c2afe4a90fbcfc170d169571f36259b1
Zyo86 <zyothebest86@gmail.com>
2021-09-17 15:00:10 +0530

    Made carousel with vanilla js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('hello!')
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+prevBtn.addEventListener('click', movePrev);
+nextBtn.addEventListener('click', moveNext);
+
+const ImageSlide = document.querySelector('.carousel_slide');
+const Images = document.querySelectorAll('.carousel_slide img');
+let currentImage = 0;
+let size = Images[0].clientWidth;
+console.log(size);
+function movePrev() {
+  console.log(currentImage);
+  if (currentImage !== 0) {
+    currentImage--;
+    ImageSlide.style.transform = `translateX(${-size * currentImage}px)`;
+  }
+}
+
+function moveNext() {
+  console.log(currentImage);
+  if (currentImage !== Images.length - 2) {
+    currentImage++;
+    ImageSlide.style.transform = `translateX(${-size * currentImage}px)`;
+  }
+}
